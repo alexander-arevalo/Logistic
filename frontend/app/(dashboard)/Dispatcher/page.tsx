8492876214582a3003a62edf7b0ff20524f6cfdb
Zero <zanderarevalo20@gmail.com>
2025-03-10 09:21:07 +0800

    updating frontend layout

---
+++ frontend/app/(dashboard)/Dispatcher/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Dispatcher() {
+  return (
+    <div>Dispatcher</div>
+  )
+}
+
+export default Dispatcher
--- frontend/app/user/Outbound/page.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Outbound = () => {
-  return (
-    <div>Outbound</div>
-  )
-}
-
-export default Outbound
